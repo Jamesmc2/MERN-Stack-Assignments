aa1556e5935816b0a167a2d3a22b5758a0cfef07
jamesmc2 <jamesmc21998@gmail.com>
2020-03-05 13:41:18 -0600

    more forms

--- react/more_forms/src/App.js
+++ react/more_forms/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import RegisterForm from './components/RegisterForm'
+
 function App() {
   return (
     <div className="App">
--- react/more_forms/src/components/RegisterForm.js
+++ react/more_forms/src/components/RegisterForm.js
@@ -22,19 +22,24 @@ const RegisterForm = props => {
             <form onSubmit={onSubmitHandler}>
             <label>First Name</label>
                 <input type="text" name="firstName" onChange={onChangeHandler}/>
-                <br/>        
+                <br/>
+                {formState.firstName.length < 2 ? <p>First name must be 2 characters long!</p>:<p></p>}
                 <label>Last Name</label>
                 <input type="text" name="lastName" onChange={onChangeHandler}/>
-                <br/>        
+                <br/>     
+                {formState.lastName.length < 2 ? <p>Last name must be 2 characters long!</p>:<p></p>}   
                 <label>Email</label>
                 <input type="email" name="email" onChange={onChangeHandler}/>
-                <br/>        
+                <br/>   
+                {formState.email.length < 5 ? <p>Email must be 5 characters long.</p>:<p></p>}     
                 <label>Password</label>
                 <input type="password" name="password" onChange={onChangeHandler}/>
-                <br/>        
+                <br/>     
+                {formState.password.length < 8 ? <p>Password must be 8 characters long.</p>:<p></p>}
                 <label>Confirm Password</label>
                 <input type="password" name="confirmPassword" onChange={onChangeHandler}/>
                 <br/>   
+                {formState.password !== formState.confirmPassword ? <p>Passwords must match!.</p>:<p></p>}
                 <input type="submit" />     
                 <p>First Name: {formState.firstName}</p>
                 <p>Last Name: {formState.lastName}</p>
